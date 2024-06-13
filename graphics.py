d34db70da7c350f274e1c881f40d9d7265d75450
montymalik <monty.malik@gmail.com>
2024-06-13 10:18:49 -0400

    first commit

---
+++ graphics.py
@@ -0,0 +1,48 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Simple GUI")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
+        self.__canvas.pack(fill=BOTH, expand=True)
+        self.__running = False
+
+    def draw_line(self,line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("window closed...")
+
+    def close(self):
+        self.__running = False
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2)
+
+
+
+
+
+
+
